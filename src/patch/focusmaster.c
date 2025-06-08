1a532027d7443d97da6f891f5f06b30b325e2db1
seraphgrid <90430427+seraphgrid@users.noreply.github.com>
2025-06-07 20:17:46 -0400

    Add files via upload

---
+++ src/patch/focusmaster.c
@@ -0,0 +1,29 @@
+void
+focusmaster(const Arg *arg)
+{
+	Client *master;
+	Monitor *m = selmon;
+	int i;
+
+	if (m->nmaster < 1)
+		return;
+	if (!m->sel || (m->sel->isfullscreen && lockfullscreen))
+		return;
+
+	master = nexttiled(m->clients);
+
+	if (!master)
+		return;
+
+	for (i = 0; !(m->tagset[m->seltags] & 1 << i); i++);
+	i++;
+
+	if (m->sel == master) {
+		if (m->tagmarked[i] && ISVISIBLE(m->tagmarked[i]))
+			focus(m->tagmarked[i]);
+	} else {
+		m->tagmarked[i] = m->sel;
+		focus(master);
+	}
+	restack(m);
+}
